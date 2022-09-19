2846c5d114fca1e68785b20a04e27256ea551fee
Sandrala Bhavana <bhavanasandrala@HweServer.mirafra.com>
2022-09-20 04:54:46 +0530

    shallow

--- copy/shallow_copy/shallow_copy.sv
+++ copy/shallow_copy/shallow_copy.sv
@@ -2,53 +2,53 @@
 //Shallow copy:
 //
 //-------------------------------------------------------
-class data;
-  int a;
-    int b ;
-      function new();   
-        a = 25;
-         b = 56;
-      endfunction
+class company;
+  string a;
+  int b ;
+  function new();   
+    a = "Mirafra";
+    b = 13;
+  endfunction
 endclass
 //-- class ---  
-class pack;
-  int c;
-    int d;
-    data ar;
+class Mirafra;
+  string c;
+  int d;
+  company ar;
 
   function new();
-    c = 35;
-    d = 79;
+    c = "Teams";
+    d = 8;
     ar = new();
   endfunction
 
   function void display();
-    $display("\t c=%0d",c);
-    $display("\t d=%0d",d);
     $display("\t a=%0d",ar.a);
     $display("\t b=%0d",ar.b);
+    $display("\t c=%0d",c);
+    $display("\t d=%0d",d);
   endfunction
 endclass
 
 module shallow;
-pack p1;
-pack p2;
+Mirafra p1;
+Mirafra p2;
 initial begin
   p1 =new();
   $display("contents of p1");
   p1.display();
-  
+
   p2 =new p1;
- $display("contents of p2");
-   p2.display();
-   p2.c=56;
-  p2.ar.a=60;
-  p2.ar.b=90;
+  $display("contents of p2");
+  p2.display();
+  p2.c="BJT";
+  p2.ar.a="Team3";
+  p2.ar.b=4;
   $display("diplay contents of p1");
   p1.display();
   $display("diplay contents of p2");
- p2.display();
+  p2.display();
 end
-  endmodule
+endmodule
 
 
