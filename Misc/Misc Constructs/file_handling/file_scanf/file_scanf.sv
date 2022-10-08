d949f88cd8b078ddc02472c02d775ac0ebdc3699
Sandrala Bhavana <bhavanasandrala@HweServer.mirafra.com>
2022-10-09 00:51:58 +0530

    file

---
+++ Misc/Misc Constructs/file_handling/file_scanf/file_scanf.sv
@@ -0,0 +1,40 @@
+module scanf;
+ 
+ int fd;
+ int i;
+ string str,str2;
+
+  initial begin
+
+    fd = $fopen ("hihihi.sv", "w");
+
+      $fdisplay(fd,"bhavana");
+      $fdisplay(fd,"sandrala");
+      $fdisplay(fd,"c");
+
+    $fclose(fd);
+
+    fd = $fopen("hihihi.sv","r");
+
+//Usage of fscanf
+   $fscanf(fd,"str=%0s",str);
+    repeat(2)
+       begin
+        $fgets(str,fd);
+          $display("contents of fscanf");
+          $display("str = %0s",str);
+        end
+
+//Usage of sscanf
+    $display("contents of sscanf");
+    $fgets(str,fd);
+    $sscanf(str,"%0s",str2);
+      
+      $display("str = %0s",str);
+      $display("str2 = %0s",str2);
+      
+    $fclose(fd);
+
+  end
+
+endmodule
---
+++ Misc/Misc Constructs/file_handling/file_scanf/hihihi.sv
@@ -0,0 +1,3 @@
+bhavana
+sandrala
+c
